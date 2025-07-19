1dc58e0d5f69f3002b54cad9942cf402d2db93cb
JiMare <zoa@seznam.cz>
2025-07-19 15:25:08 +0200

    day6 part2

--- js/days/day6.js
+++ js/days/day6.js
@@ -2,47 +2,74 @@ const getData = require("../index");
 
 const data = getData("../data/day6.txt");
 
-const matrix = data.map((row) => row.split(""));
+const baseMatrix = data.map((row) => row.split(""));
 
 const guard = "^";
 
-const guardStartRow = matrix.find((row) => row.indexOf("^") !== -1);
-const guardStartRowIndex = matrix.indexOf(guardStartRow);
+const guardStartRow = baseMatrix.find((row) => row.indexOf(guard) !== -1);
+const guardStartRowIndex = baseMatrix.indexOf(guardStartRow);
 const guardStartIndex = guardStartRow.indexOf(guard);
+
 const directions = ["up", "right", "down", "left"];
 const startGuardDirection = directions[0];
 
-const part1 = () => {
+const getIsFreeWay = (matrix, currentDirection, rowIndex, guardIndex) => {
+  if (currentDirection === "up") {
+    const row = matrix[rowIndex - 1];
+    return !row || row[guardIndex] !== "#";
+  } else if (currentDirection === "down") {
+    const row = matrix[rowIndex + 1];
+    return !row || row[guardIndex] !== "#";
+  } else if (currentDirection === "left") {
+    return (
+      !matrix[rowIndex][guardIndex - 1] ||
+      matrix[rowIndex][guardIndex - 1] !== "#"
+    );
+  } else {
+    return (
+      !matrix[rowIndex][guardIndex + 1] ||
+      matrix[rowIndex][guardIndex + 1] !== "#"
+    );
+  }
+};
+
+const getPositions = (matrix) => {
   const positions = new Map();
   let newGuardRowIndex = guardStartRowIndex;
   let newGuardIndex = guardStartIndex;
   let newGuardDirection = startGuardDirection;
+
   while (matrix[newGuardRowIndex] && matrix[newGuardRowIndex][newGuardIndex]) {
     const key = `${newGuardRowIndex},${newGuardIndex}`;
-    const count = positions.get(key) ?? 0;
-    positions.set(key, count + 1);
-    let isFreeWay = true;
+    const positionDirections = positions.get(key);
+    if (positionDirections && positionDirections.includes(newGuardDirection)) {
+      return;
+    }
+    positions.set(
+      key,
+      positionDirections
+        ? [...positionDirections, newGuardDirection]
+        : [newGuardDirection]
+    );
+
     //look for free way
-    if (newGuardDirection === "up") {
-      const row = matrix[newGuardRowIndex - 1];
-      isFreeWay = !row || row[newGuardIndex] !== "#";
-    } else if (newGuardDirection === "down") {
-      const row = matrix[newGuardRowIndex + 1];
-      isFreeWay = !row || row[newGuardIndex] !== "#";
-    } else if (newGuardDirection === "left") {
-      isFreeWay =
-        !matrix[newGuardRowIndex][newGuardIndex - 1] ||
-        matrix[newGuardRowIndex][newGuardIndex - 1] !== "#";
-    } else {
-      isFreeWay =
-        !matrix[newGuardRowIndex][newGuardIndex + 1] ||
-        matrix[newGuardRowIndex][newGuardIndex + 1] !== "#";
+    let isFreeWay = false;
+    while (!isFreeWay) {
+      const resultIsFreeWay = getIsFreeWay(
+        matrix,
+        newGuardDirection,
+        newGuardRowIndex,
+        newGuardIndex
+      );
+      //if not free way turn
+      if (!resultIsFreeWay) {
+        newGuardDirection =
+          directions[(directions.indexOf(newGuardDirection) + 1) % 4];
+      } else {
+        isFreeWay = true;
+      }
     }
-    //if not free way turn
-    if (!isFreeWay) {
-      newGuardDirection =
-        directions[(directions.indexOf(newGuardDirection) + 1) % 4];
-    }
+
     //move
     if (newGuardDirection === "up") {
       newGuardRowIndex--;
@@ -54,8 +81,39 @@ const part1 = () => {
       newGuardIndex++;
     }
   }
+  return positions;
+};
 
+const part1 = () => {
+  const positions = getPositions(baseMatrix);
   return positions.size;
 };
 
+const part2 = () => {
+  const positions = getPositions(baseMatrix);
+
+  let infiniteLoop = 0;
+
+  const matrixWithObstacleArr = [];
+
+  for (const key of positions.keys()) {
+    const [row, col] = key.split(",");
+    const copyMatrix = baseMatrix.map((row) => [...row]);
+    if (copyMatrix[row][col] !== guard) {
+      copyMatrix[row][col] = "#";
+      matrixWithObstacleArr.push(copyMatrix);
+    }
+  }
+
+  matrixWithObstacleArr.forEach((matrixWithObstacle, index) => {
+    const result = getPositions(matrixWithObstacle);
+    if (!result) {
+      infiniteLoop++;
+    }
+  });
+
+  return infiniteLoop;
+};
+
 console.log(part1()); //4696
+console.log(part2()); //1443
